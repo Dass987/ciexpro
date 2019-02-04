02d271850dfe3880cfb668fd8586264298455691
Dass987 <sequeradanniel@gmail.com>
2019-02-04 16:48:27 -0400

    Main layout redisgned. Colucci is new home. Template in standby

--- js/main.js
+++ js/main.js
@@ -5,16 +5,24 @@ window.addEventListener('load', () => {
 });
 
 window.addEventListener('scroll', () => {
+
 	
 	if (document.getElementsByTagName("html")[0].scrollTop >= 620) {
-		document.getElementById("main-navbar").style.height = "60px";
-		document.getElementById("main-navbar").style.backgroundColor = "#fafafa";
+		/*document.getElementById("main-navbar").style.height = "60px";
+		document.getElementById("main-navbar").style.backgroundColor = "#fafafa";*/
+		//document.querySelector(".features-first").style.borderRight = "1px solid #512da8";
 	} else if (document.getElementsByTagName("html")[0].scrollTop < 620) {
-		document.getElementById("main-navbar").style.height = "100px";
-		document.getElementById("main-navbar").style.backgroundColor = "transparent";
+		//document.querySelector(".features-first").style.borderColor = "#e0e0e0";
+		/*document.getElementById("main-navbar").style.height = "100px";
+		document.getElementById("main-navbar").style.backgroundColor = "transparent";*/
 	}
 
 });
+
+/* Set the width of the side navigation to 250px */
+document.querySelector(".sidenav-trigger").addEventListener('click', () => { document.getElementById("ciexpro-sidenav").style.width = "250px"; });
+/* Set the width of the side navigation to 0 */
+document.querySelector(".close-sidenav").addEventListener("click", () => { document.getElementById("ciexpro-sidenav").style.width = "0"; });
 
 // ---------------------------------------------------------------------------------------------------------------------------- //
 
