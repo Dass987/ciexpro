a3c4001526bd2b0dbc0141e71c80481cff7b7769
Dass987 <sequeradanniel@gmail.com>
2019-02-08 08:55:13 -0400

    Ciexpro template v0.7.4. Working on image preloader.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,64 @@
 let myVar = setTimeout(showPage, 2000);
+const images = {
+	south: {
+		src: [
+			'../img/south/DSC_8114.jpg',
+			'../img/south/DSC_8115.jpg',
+			'../img/south/DSC_8116.jpg',
+			'../img/south/DSC_8119.jpg'
+		]
+	},
+	north: {
+		src: [
+			'../img/north/DSC_8137.jpg',
+			'../img/north/DSC_8141.jpg',
+			'../img/north/DSC_8146.jpg',
+			'../img/north/DSC_8157.jpg'
+		]
+	},
+	base: {
+		src: [
+			'../img/base/DSC_8173.jpg',
+			'../img/base/DSC_8176.jpg',
+			'../img/base/DSC_8177.jpg',
+			'../img/base/DSC_8189.jpg',
+			'../img/base/DSC_8191.jpg',
+			'../img/base/DSC_8193.jpg'
+		]
+	}
+};
+
+const northImgs = document.querySelectorAll("#north-route img");
+const southImgs = document.querySelectorAll("#south-route img");
+const baseImgs = document.querySelectorAll("#base-route img");
+let counter = 0;
 
 window.addEventListener('load', async () => {
+	
+	images.north.src.forEach(uri => {
+
+		northImgs[counter].src = uri;
+		counter++;
+
+	});
+
+	counter = 0;
+
+	images.south.src.forEach(uri => {
+
+		southImgs[counter].src = uri;
+		counter++;
+
+	});
+
+	counter = 0;
+
+	images.base.src.forEach(uri => {
+
+		baseImgs[counter].src = uri;
+		counter++;
+
+	});
 
 	let north = await $('.gallery a.north-route-thumbnail').simpleLightbox(),
 		south = await $('.gallery a.south-route-thumbnail').simpleLightbox(),
